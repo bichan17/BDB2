11d1c3a6cbe7edb175b3d06bfca4f62c102e9cc1
Eric Bichan <eric.bichan@gmail.com>
2014-04-13 23:24:01 -0400

    now with jekyll

---
+++ _posts/2013-04-20-project-sticky-waffle.markdown
@@ -0,0 +1,8 @@
+---
+layout: project
+title: Project Sticky Waffle
+categories: work
+caption: A music synthesizer built with Web Audio API and HTML Canvas
+---
+
+yes hello this is STICKY WAFFLE!
---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+//tutorial
+//http://www.webappers.com/2013/02/08/how-to-make-sprite-animations-with-html5-canvas/
+
+app.main = (function(){
+    function init() {
+    console.log("init");
+  }
+  
+  
+
+  //Public interface
+  return{
+    init : init
+    //someVar : someVar,
+    //someFunc : someFunc
+  }
+})();
